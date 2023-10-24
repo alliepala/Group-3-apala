865499f5693d444421b9a5456448b024d01ad748
alliepala <123181593+alliepala@users.noreply.github.com>
2023-10-24 10:26:34 -0400

    Update sum_even_fibonacci.py
    added comments

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,9 +1,15 @@
+# This is a file that declares ints a and b 
 a, b = 0, 1
+
+# initialize sum_even to add all the even fibonacci values
 sum_even = 0
+
+# if the value is less that 4000000 and is even, add the values to sum_even
 while b < 4000000:
     if b % 2 == 0:
         sum_even += b
     a, b = b, a+b
+
 
 print(sum_even)
 
